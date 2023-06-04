d3666757a27724f23df6e5a0bf78f924b9fabf13
TomTom <93038247+AverageGithub@users.noreply.github.com>
2023-06-04 14:47:07 +0200

    Initial commit

---
+++ src/main/java/hu/rivalsnetwork/rivalstemplate/RivalsTemplatePlugin.java
@@ -0,0 +1,21 @@
+package hu.rivalsnetwork.rivalstemplate;
+
+import hu.rivalsnetwork.rivalsapi.plugin.RivalsPluginImpl;
+
+public class RivalsTemplatePlugin extends RivalsPluginImpl {
+
+    @Override
+    public void enable() {
+        // Enable code
+    }
+
+    @Override
+    public void disable() {
+        // Disable code
+    }
+
+    @Override
+    public void reload() {
+        // Reload code
+    }
+}
